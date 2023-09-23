384e4f1dd935392efabfa0658d82c236d2556d80
KauanAmorim <kauanamorim88@gmail.com>
2023-09-23 16:20:23 -0300

    adicioando pattern repository e o adicionando em casos de uso e testes

---
+++ src/app/use-cases/send-notification.spec.ts
@@ -0,0 +1,24 @@
+import { Notification } from '../entities/notification';
+import { SendNotification } from './send-notification';
+
+const notifications: Notification[] = [];
+
+const NotificationsRepository = {
+  async create(notification: Notification) {
+    notifications.push(notification);
+  },
+};
+
+describe('Send Notification', () => {
+  it('should be able to send a notification', async () => {
+    const sendNotification = new SendNotification(NotificationsRepository);
+    await sendNotification.execute({
+      content: 'This is a notification',
+      category: 'market',
+      recipientId: 'example-recipient-id',
+    });
+
+    console.log(notifications);
+    expect(notifications).toHaveLength(1);
+  });
+});
---
+++ src/app/use-cases/send-notification.ts
@@ -0,0 +1,32 @@
+import { Content } from '../entities/content';
+import { Notification } from '../entities/notification';
+import { NotificationsRepository } from '../repositories/notifications-repository';
+
+interface SendNotificationRequest {
+  recipientId: string;
+  content: string;
+  category: string;
+}
+
+interface SendNotificationResponse {
+  notification: Notification;
+}
+
+export class SendNotification {
+  constructor(private notificationRepository: NotificationsRepository) {}
+
+  async execute(
+    request: SendNotificationRequest,
+  ): Promise<SendNotificationResponse> {
+    const { recipientId, content, category } = request;
+    const notification = new Notification({
+      recipientId,
+      content: new Content(content),
+      category,
+    });
+
+    await this.notificationRepository.create(notification);
+
+    return { notification };
+  }
+}
